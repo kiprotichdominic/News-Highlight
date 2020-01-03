9de42afe17fd143a7d49a60aaa83b9a866401a26
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-03 22:31:39 +0300

    create a get sources function

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,5 @@
 from app import app
-from app.models.news import Articles, News
+from .models import Articles, News
 import requests
 
 # Getting api key
@@ -10,3 +10,18 @@ base_url = app.config["NEWS_API_BASE_URL"]
 
 #Getting the articles base url
 article_base_url = app.config["ARTICLE_API_BASE_URL"]
+
+def get_sources():
+    '''
+    Function that gets the json response to our url request
+    '''
+
+    get_news_url = base_url + api_key
+
+    get_news_response = requests.get(get_news_url).json()
+
+    if get_news_response['sources']:
+        news_results_list = get_news_response['sources']
+        news_results = process_results(news_results_list)
+
+        return news_results
