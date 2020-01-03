e187bbe3cf95fd3b5b3785c4ab5185deefe0db62
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-03 22:24:23 +0300

    initialised the application

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''    
+    title = 'Home - Welcome to The best News  Highlight Website Online'
+    return render_template('index.html', title = title)
+
+
+
+@app.route('/articles/<id>')
+def news(id):
+    return render_template('articles.html')
+
