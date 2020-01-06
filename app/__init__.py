d48e30a9db776ee5da623131f08e2baca30d8d55
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-06 16:56:05 +0300

    final push

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from .config import DevConfig
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+app = Flask(__name__, instance_relative_config = True)
 
-# Setting up configuration
+#setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
 
 from app import views
